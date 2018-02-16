a277a6a738f953a3af18fb8af632e5830dd3c7df
benjamin <benjamin.balanger@epitech.eu>
2018-02-16 17:08:07 +0100

    Level3 + Fix Level2

--- level2/main.rb
+++ level2/main.rb
@@ -34,18 +34,17 @@ class Main
     price_per_day = @cars[rental['car_id']]['price_per_day']
     duration = (Date.parse(rental['end_date']) - Date.parse(rental['start_date'])).to_i + 1
     durationPrice = 0
-    Array.new(duration).each_with_index do |_, index|
-      case index
-        when 0 .. 3
-          durationPrice += price_per_day * 0.9
-        when 4 .. 9
-          durationPrice += price_per_day * 0.7
-        when index > 10
+    (Array.new(duration)).each_with_index{ |_, index|
+        if index >= 10
           durationPrice += price_per_day * 0.5
+        elsif index >= 4
+          durationPrice += price_per_day * 0.7
+        elsif index >= 1
+          durationPrice += price_per_day * 0.9
         else
           durationPrice += price_per_day
       end
-    end
+    }
     durationPrice.to_i
   end
 end
